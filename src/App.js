3f45c0ac0106bd3212152b5ed916d9cf241de045
Minsung (Nicholas) Suh <didauds@gmail.com>
2022-08-28 00:21:47 -0600

    Initial commit for Project

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
+import Navbar from "./components/Navbar";
+
+// 'rfce' to create snippet
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;

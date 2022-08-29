6581828943a8302d384909382354630dd340fac3
Minsung (Nicholas) Suh <didauds@gmail.com>
2022-08-29 11:00:03 -0600

    Add vertical timeline for Experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -5,11 +5,38 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 
+import SchoolIcon from "@material-ui/icons/School";
+import WorkIcon from "@material-ui/icons/Work";
+
 function Experience() {
   return (
     <div className="experience">
-      <VerticalTimeline>
-        <VerticalTimelineElement></VerticalTimelineElement>
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2010 - 2014"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Seneca College, Toronto, Ontario
+          </h3>
+          <p>College Diploma</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2010 - 2014"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Front End Developer - Kismet AI
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Toronto, Ontario
+          </h4>
+          <p>Developed something</p>
+        </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
   );

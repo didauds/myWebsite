c05b8000ac54c2992a9cdad7d7eb17b1a75b93cd
Minsung (Nicholas) Suh <didauds@gmail.com>
2022-09-04 14:29:21 -0600

    Add Project details

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -4,9 +4,11 @@ export const ProjectList = [
   {
     name: "Social Media Website",
     image: "",
+    skills: "",
   },
   {
     name: "GitHub API",
     image: "",
+    skills: "React, React Context",
   },
 ];
